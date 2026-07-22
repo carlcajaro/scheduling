5055df8e386532e25d6bd40f6fffbc3d7eb04a29
carlcajaro <carlcajaro@gmail.com>
2026-07-23 07:28:38 +0800

    Create logout.php

---
+++ logout.php
@@ -0,0 +1,26 @@
+<?php
+require_once 'config.php';
+
+// Log the logout activity before clearing session
+if (isset($_SESSION['username'])) {
+    logActivity($pdo, $_SESSION['username'], 'LOGOUT');
+}
+
+// Unset all session variables
+$_SESSION = [];
+
+// Destroy session cookie if set
+if (ini_get("session.use_cookies")) {
+    $params = session_get_cookie_params();
+    setcookie(session_name(), '', time() - 42000,
+        $params["path"], $params["domain"],
+        $params["secure"], $params["httponly"]
+    );
+}
+
+// Destroy session
+session_destroy();
+
+// Redirect to login page
+header("Location: login.php");
+exit;
